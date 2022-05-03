22c7771c022564aa6d006c0434d88f49bd92f892
juhye2 <102467068+juhye2@users.noreply.github.com>
2022-05-03 15:32:54 +0900

    1871210 이주혜 / 주간 달련 어댑터

---
+++ app/src/main/java/com/example/monthview/WeekCalendarAdapter.java
@@ -0,0 +1,35 @@
+package com.example.monthview;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import java.util.Calendar;
+
+public class WeekCalendarAdapter extends FragmentStateAdapter {
+    private static int NUM_ITEMS=100;
+    public WeekCalendarAdapter(@NonNull WeekViewFragment fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    int year = Calendar.getInstance().get(Calendar.YEAR);
+    int month = Calendar.getInstance().get(Calendar.MONTH);
+    int day;
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        // year 변수에 저장된 값을 Year에, month변수에 저장된 값을 Month에, 1을 Day에 넣어줌
+        int dm = position*7/42;
+        int realMonth = (month+dm)%12+1;
+        int ym = (month+dm)/12;
+        day = position;
+        return WeekCalendarFragment.newInstance((year+ym), realMonth, day);
+    }
+
+    @Override
+    public int getItemCount() {
+        return NUM_ITEMS;
+    }
+
+}
